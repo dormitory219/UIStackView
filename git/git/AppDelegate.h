aa3730ba4e01866630d121abd56807b79c671fe5
dormitory219 <289067005@qq.com>
2016-01-25 14:48:34 +0800

    initi commint

---
+++ git/git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by 余强 on 16/1/25.
+//  Copyright © 2016年 bdcluster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by 余强 on 16/1/25.
+//  Copyright © 2016年 bdcluster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
